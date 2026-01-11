3575268479c41b079df6742833dc07ad1d3c0699
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-01-11 20:28:47 +0100

    refactor: use `nativeUuid` for UUID columns in `PlaytimeSessionsTable`

--- surf-playtime-fallback/src/main/kotlin/dev/slne/surf/playtime/fallback/table/PlaytimeSessionsTable.kt
+++ surf-playtime-fallback/src/main/kotlin/dev/slne/surf/playtime/fallback/table/PlaytimeSessionsTable.kt
@@ -1,11 +1,12 @@
 package dev.slne.surf.playtime.fallback.table
 
+import dev.slne.surf.database.columns.nativeUuid
 import dev.slne.surf.database.libs.org.jetbrains.exposed.v1.core.dao.id.LongIdTable
 import dev.slne.surf.database.libs.org.jetbrains.exposed.v1.javatime.datetime
 
 object PlaytimeSessionsTable : LongIdTable("playtime_sessions") {
-    val sessionUuid = uuid("session_uuid").uniqueIndex()
-    val playerUuid = uuid("playtime_player_id")
+    val sessionUuid = nativeUuid("session_uuid").uniqueIndex()
+    val playerUuid = nativeUuid("playtime_player_id")
     val serverName = varchar("server_name", 255)
     val serverCategory = varchar("server_category", 255)
     val startTime = datetime("start_time")
