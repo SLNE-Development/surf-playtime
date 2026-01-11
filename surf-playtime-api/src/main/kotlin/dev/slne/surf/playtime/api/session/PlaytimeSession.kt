36263235de007e8f9d88b76f6aaf65c7fd7260e8
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-01-11 20:30:49 +0100

    refactor: remove `@Serializable` and `@Contextual` annotations from `PlaytimeSession` fields

--- surf-playtime-api/src/main/kotlin/dev/slne/surf/playtime/api/session/PlaytimeSession.kt
+++ surf-playtime-api/src/main/kotlin/dev/slne/surf/playtime/api/session/PlaytimeSession.kt
@@ -1,19 +1,16 @@
 package dev.slne.surf.playtime.api.session
 
-import kotlinx.serialization.Contextual
-import kotlinx.serialization.Serializable
 import java.time.Duration
 import java.time.LocalDateTime
 import java.util.*
 
-@Serializable
 data class PlaytimeSession(
-    val playerUuid: @Contextual UUID,
-    val sessionId: @Contextual UUID,
+    val playerUuid: UUID,
+    val sessionId: UUID,
     val server: String,
     val category: String,
-    val startTime: @Contextual LocalDateTime,
-    var endTime: @Contextual LocalDateTime
+    val startTime: LocalDateTime,
+    var endTime: LocalDateTime
 ) {
     val durationSeconds get() = Duration.between(startTime, endTime).seconds
 }
